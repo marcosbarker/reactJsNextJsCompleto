165057f911487d29299d6a478b7a5e25499fbf37
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-17 21:40:59 -0300

    chore: lesson15

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -20,21 +21,47 @@ class App extends Component {
         body: 'Corpo 3'
       }
     ]
-
-
   };
+
+  timeoutUpdate = null;
+
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillMount() {
+    clearTimeout(this.clearTimeout);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'Mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 1000);
+
+
+  }
 
 
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <p>{counter}</p>
 
         {posts.map(post => (
           <div>
             <h1>{post.title}</h1>
+            <p>{post.body}</p>
           </div>
 
 
